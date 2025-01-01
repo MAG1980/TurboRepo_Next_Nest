f358fde7c99968806c4fdc3c4509597f0ab67c00
MAG1980 <al-1980@inbox.ru>
2025-01-02 00:03:02 +0300

    Исправлены стили SignupForm.
    1

--- apps/web/app/auth/signup/signupForm.tsx
+++ apps/web/app/auth/signup/signupForm.tsx
@@ -4,18 +4,18 @@ import { SubmitButton } from "@/components/ui/submitButton";
 
 export const SignupForm = () => {
   return (
-    <form>
-      <div className="flex flex-col gap-2">
+    <form className="flex flex-col gap-2">
+      <div>
         <Label htmlFor="name">Name</Label>
-        <Input id="name" type="text" name="name" placeholder="Name"/>
+        <Input id="name" type="text" name="name" placeholder="username"/>
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div>
         <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" name="email" placeholder="Email"/>
+        <Input id="email" type="email" name="email" placeholder="username@email.com"/>
       </div>
 
-      <div className="flex flex-col gap-2">
+      <div>
         <Label htmlFor="password">Password</Label>
         <Input id="password" type="password" name="password" placeholder="Password"/>
       </div>
