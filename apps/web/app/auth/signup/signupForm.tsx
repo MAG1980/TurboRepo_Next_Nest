256ac7aac9832810f718b136c124c124d6620b23
MAG1980 <al-1980@inbox.ru>
2025-01-02 03:15:30 +0300

    Настроена обработка формы регистрации с помощью хука useActionState. В SignupForm реализован вывод сообщений с ошибками валидации.
    Хук useFormState заменён на useActionState.

--- apps/web/app/auth/signup/signupForm.tsx
+++ apps/web/app/auth/signup/signupForm.tsx
@@ -1,24 +1,57 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { SubmitButton } from "@/components/ui/submitButton";
+'use client';
+import { useActionState } from 'react';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { SubmitButton } from '@/components/ui/submitButton';
+import { signUp } from '@/lib/serverActions';
 
 export const SignupForm = () => {
+  const [state, action] = useActionState(signUp, undefined);
   return (
-    <form className="flex flex-col gap-2">
+    <form action={action} className="flex flex-col gap-2">
+      {state?.message && (
+        <p className="text-sm text-red-500">{state.message}</p>
+      )}
       <div>
         <Label htmlFor="name">Name</Label>
-        <Input id="name" type="text" name="name" placeholder="username"/>
+        <Input id="name" type="text" name="name" placeholder="username" />
       </div>
+      {state?.error?.name && (
+        <p className="text-sm text-red-500">{state.error.name}</p>
+      )}
 
       <div>
         <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" name="email" placeholder="username@email.com"/>
+        <Input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="username@email.com"
+        />
       </div>
+      {state?.error?.email && (
+        <p className="text-sm text-red-500">{state.error.email}</p>
+      )}
 
       <div>
         <Label htmlFor="password">Password</Label>
-        <Input id="password" type="password" name="password" placeholder="Password"/>
+        <Input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
       </div>
+      {state?.error?.password && (
+        <div className="text-sm text-red-500">
+          <p>Password must:</p>
+          <ul>
+            {state.error.password.map((message) => (
+              <li key={message}>{message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
 
       <SubmitButton>Sgn Up</SubmitButton>
     </form>
